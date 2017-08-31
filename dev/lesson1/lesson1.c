199d442408fcba8021922d7cb5d168cd4838beeb
Austin Krauss <austin.krauss@gmail.com>
2017-08-30 22:52:20 -0700

    minor formatting changes based on personal preference

--- dev/lesson1/lesson1.c
+++ dev/lesson1/lesson1.c
@@ -13,48 +13,53 @@
 //	Globals
 unsigned char index;
 
-const unsigned char TEXT[]={
-"Hello World!"};
+const unsigned char TEXT[] = 
+{
+	"Hello World!"
+};
 
-const unsigned char PALETTE[]={
-0x1f, 0x00, 0x10, 0x20
-}; //	black, gray, lt gray, white
+const unsigned char PALETTE[] =
+{
+	0x1f, // black
+	0x00, // gray
+	0x10, // light gray
+	0x20  // white
+}; 
 
-	
-	
-	
-
-void main (void) {
-	//	turn off the screen
+void main ()
+{
+	// turn off the screen
 	PPU_CTRL = 0;
 	PPU_MASK = 0;
 	
-	//	load the palette
-	PPU_ADDRESS = 0x3f; 	//	set an address in the PPU of 0x3f00
+	// load the palette
+	PPU_ADDRESS = 0x3f; 	// set an address in the PPU of 0x3f00
 	PPU_ADDRESS = 0x00;
 	
-	for(index = 0; index < sizeof(PALETTE); ++index){
+	for(index = 0; index < sizeof(PALETTE); ++index)
+	{
 		PPU_DATA = PALETTE[index];
 	}
 		
 	//load the text
-	PPU_ADDRESS = 0x21;  	//	set an address in the PPU of 0x21ca
-	PPU_ADDRESS = 0xca;  	//	about the middle of the screen
-	for( index = 0; index < sizeof(TEXT); ++index ){
+	PPU_ADDRESS = 0x21;  	// set an address in the PPU of 0x21ca
+	PPU_ADDRESS = 0xca;  	// about the middle of the screen
+	for(index = 0; index < sizeof(TEXT); ++index)
+	{
 		PPU_DATA = TEXT[index];
 	}
 		
-	//	reset the scroll position	
+	// reset the scroll position	
 	PPU_ADDRESS = 0;
 	PPU_ADDRESS = 0;
 	SCROLL = 0;
 	SCROLL = 0;
 	
-	//	turn on screen
-	PPU_CTRL = 0x90; 	//	screen is on, NMI on
+	// turn on screen
+	PPU_CTRL = 0x90; 	// screen is on, NMI on
 	PPU_MASK = 0x1e;
 	
-	//	infinite loop
+	// infinite loop
 	while (1);
 };
 	
